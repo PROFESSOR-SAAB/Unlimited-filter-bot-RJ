e3703da3ad3752931d071e7f20eb22898c79f15e
PROFESSOR-SAAB <109969024+PROFESSOR-SAAB@users.noreply.github.com>
2022-08-15 11:21:01 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -51,8 +51,8 @@ async def cb_handler(client, query):
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("🔰Join Group🔰", url="https://t.me/malluinstaufollowers"),
-                    InlineKeyboardButton("details about Group", url="https://t.me/Malluinstabot")
+                    InlineKeyboardButton("🔰Join Group🔰", url="https://t.me/PUNJABI_FILMY_PITAARA_786"),
+                    InlineKeyboardButton("details about Group", url="https://t.me/+yuMu7Eg3jUlkY2M1")
                 ]
             ]
         )
